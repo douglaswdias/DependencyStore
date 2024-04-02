d9ff18c2fdcbf9cf321aa6e585a50323dff29e11
Douglas Willian Dias <douglas.wdias@hotmail.com>
2024-04-01 21:18:06 -0300

    Initial Commit

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using DependencyStore.Models;
+using DependencyStore.Repositories.Contracts;
+using DependencyStore.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using RestSharp;
+
+namespace DependencyStore.Controllers;
+
+public class OrderController : ControllerBase
+{
+    private readonly ICustomerRepository _customerRepository;
+    private readonly IDeliveryFeeService _deliveryFeeService;
+    private readonly IPromoCodeRepository _promoCodeRepository;
+
+    public OrderController(
+        ICustomerRepository customerRepository, 
+        IDeliveryFeeService deliveryFeeService, 
+        IPromoCodeRepository promoCodeRepository)
+    {
+        _customerRepository = customerRepository;
+        _deliveryFeeService = deliveryFeeService;
+        _promoCodeRepository = promoCodeRepository;
+    }
+    [Route("v1/orders")]
+    [HttpPost]
+    public async Task<IActionResult> Place(string customerId, string zipCode, string promoCode, int[] products)
+    {
+        // #1 - Recupera o cliente
+        var customer = await _customerRepository.GetCustomerByIdAsync(customerId);
+        if (customer == null)
+            return NotFound();
+
+        // #2 - Calcula o frete
+        var deliveryFee = await _deliveryFeeService.GetDeliveryFeeAsync(zipCode);
+        var cupom = await _promoCodeRepository.GetPromoCodeAsync(promoCode);
+        var discount = cupom?.Value ?? 0M;
+        var order = new Order(deliveryFee, discount, new List<Product>());
+
+        return Ok($"Pedido {order.Code} geredo com sucesso");
+    }
+}
---
+++ Repositories/CustomerRepository.cs
@@ -0,0 +1,18 @@
+using Dapper;
+using DependencyStore.Models;
+using DependencyStore.Repositories.Contracts;
+using Microsoft.Data.SqlClient;
+
+namespace DependencyStore.Repositories;
+
+public class CustomerRepository : ICustomerRepository
+{
+  private readonly SqlConnection _connection;
+  public CustomerRepository(SqlConnection connection) => _connection = connection;
+  public async Task<Customer?> GetCustomerByIdAsync(string customerId)
+  {    
+    const string query = "SELECT [Id], [Name], [Email] FROM CUSTOMER WHERE ID=@id";
+    return await _connection.QueryFirstOrDefaultAsync<Customer>(query, new { id = customerId });
+  }
+
+}
